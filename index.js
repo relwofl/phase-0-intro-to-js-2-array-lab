a7eec8de72f1f92fc415d63243ac9cf2fcb64d43
Luke Fowler <luciusfow@gmail.com>
2021-11-16 19:53:11 -0500

    commit tree 539d6da94f01f3bc6a54bbb53e37091f07364287

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(meow){
+    cats.push(meow)
+}
+function destructivelyPrependCat(meow){
+    cats.unshift(meow)
+}
+function destructivelyRemoveLastCat(meow){
+    cats.pop(meow)
+}
+function destructivelyRemoveFirstCat(meow){
+    cats.shift(meow)
+}
+
+function appendCat(meow){
+    return [...cats, meow]
+}
+
+function prependCat(meow){
+    return [meow, ...cats]
+}
+
+function removeLastCat(meow){
+    return cats.slice(0, cats.length -1)
+}
+
+function removeFirstCat(meow){
+    return cats.slice(1)
+}
